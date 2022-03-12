cb3a706995996210ff6795d602bc3275f1c3f54c
user <user@DESKTOP-2JGPQV5>
2022-03-12 19:03:13 +0900

    board 모델 수정

--- src/main/java/org/kh/meme/board/domain/Board.java
+++ src/main/java/org/kh/meme/board/domain/Board.java
@@ -6,6 +6,7 @@ public class Board {
 	private int boardNo;
 	private String boardType;
 	private String boardTitle;
+	private String memberId;
 	private String memberNickname;
 	private Date boardDate;
 	private Date boardReDate;
@@ -16,14 +17,14 @@ public class Board {
 	private String boardStatus;
 	
 	public Board() {}
-
-	public Board(int boardNo, String boardType, String boardTitle, String memberNickname, Date boardDate,
-			Date boardReDate, String boardContents, int boardCount, int boardLike, int boardReport,
+	public Board(int boardNo, String boardType, String boardTitle, String memberId, String memberNickname,
+			Date boardDate, Date boardReDate, String boardContents, int boardCount, int boardLike, int boardReport,
 			String boardStatus) {
 		super();
 		this.boardNo = boardNo;
 		this.boardType = boardType;
 		this.boardTitle = boardTitle;
+		this.memberId = memberId;
 		this.memberNickname = memberNickname;
 		this.boardDate = boardDate;
 		this.boardReDate = boardReDate;
@@ -33,103 +34,85 @@ public class Board {
 		this.boardReport = boardReport;
 		this.boardStatus = boardStatus;
 	}
-
 	public int getBoardNo() {
 		return boardNo;
 	}
-
 	public void setBoardNo(int boardNo) {
 		this.boardNo = boardNo;
 	}
-
 	public String getBoardType() {
 		return boardType;
 	}
-
 	public void setBoardType(String boardType) {
 		this.boardType = boardType;
 	}
-
 	public String getBoardTitle() {
 		return boardTitle;
 	}
-
 	public void setBoardTitle(String boardTitle) {
 		this.boardTitle = boardTitle;
 	}
-
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
 	public String getMemberNickname() {
 		return memberNickname;
 	}
-
 	public void setMemberNickname(String memberNickname) {
 		this.memberNickname = memberNickname;
 	}
-
 	public Date getBoardDate() {
 		return boardDate;
 	}
-
 	public void setBoardDate(Date boardDate) {
 		this.boardDate = boardDate;
 	}
-
 	public Date getBoardReDate() {
 		return boardReDate;
 	}
-
 	public void setBoardReDate(Date boardReDate) {
 		this.boardReDate = boardReDate;
 	}
-
 	public String getBoardContents() {
 		return boardContents;
 	}
-
 	public void setBoardContents(String boardContents) {
 		this.boardContents = boardContents;
 	}
-
 	public int getBoardCount() {
 		return boardCount;
 	}
-
 	public void setBoardCount(int boardCount) {
 		this.boardCount = boardCount;
 	}
-
 	public int getBoardLike() {
 		return boardLike;
 	}
-
 	public void setBoardLike(int boardLike) {
 		this.boardLike = boardLike;
 	}
-
 	public int getBoardReport() {
 		return boardReport;
 	}
-
 	public void setBoardReport(int boardReport) {
 		this.boardReport = boardReport;
 	}
-
 	public String getBoardStatus() {
 		return boardStatus;
 	}
-
 	public void setBoardStatus(String boardStatus) {
 		this.boardStatus = boardStatus;
 	}
-
 	@Override
 	public String toString() {
-		return "Board [boardNo=" + boardNo + ", boardType=" + boardType + ", boardTitle=" + boardTitle
-				+ ", memberNickname=" + memberNickname + ", boardDate=" + boardDate + ", boardReDate=" + boardReDate
-				+ ", boardContents=" + boardContents + ", boardCount=" + boardCount + ", boardLike=" + boardLike
-				+ ", boardReport=" + boardReport + ", boardStatus=" + boardStatus + "]";
+		return "Board [boardNo=" + boardNo + ", boardType=" + boardType + ", boardTitle=" + boardTitle + ", memberId="
+				+ memberId + ", memberNickname=" + memberNickname + ", boardDate=" + boardDate + ", boardReDate="
+				+ boardReDate + ", boardContents=" + boardContents + ", boardCount=" + boardCount + ", boardLike="
+				+ boardLike + ", boardReport=" + boardReport + ", boardStatus=" + boardStatus + "]";
 	}
-	
 	
 	
 }
